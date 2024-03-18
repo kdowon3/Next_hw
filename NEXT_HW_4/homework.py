fb23d51d1c0d548e60c5dd6dfec2ad3a463d365d
kdowon3 <dowon8849@gmail.com>
2024-03-18 18:01:35 +0900

    edited

--- NEXT_HW_4/homework.py
+++ NEXT_HW_4/homework.py
@@ -11,15 +11,15 @@ while True:
       guess = (min + max) // 2
       print("당신이 생각한 숫자는", guess, "입니까?")
 
-      user_input = input("제가 맞췄다면 '맞음', 제가 제시한 숫자보다 크다면 '큼', 작다면 '작음'을 입력해주세요: ")
+      user= input("제가 맞췄다면 '맞음', 제가 제시한 숫자보다 크다면 '큼', 작다면 '작음'을 입력해주세요: ")
 
-      if user_input == "맞음":
+      if user == "맞음":
             print(cycle_count, "번 만에 맞췄다")
             break
-      elif user_input == "큼":
+      elif user == "큼":
             min = guess + 1
             cycle_count += 1
-      elif user_input == "작음":
+      elif user == "작음":
             max = guess - 1
             cycle_count += 1
       else:
